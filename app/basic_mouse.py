bc3f96316a5263f7ff9b0474ca70aacc8a393ddc
Patrick Kalkman <patrick@simpletechture.nl>
2023-06-21 21:51:28 +0200

    refactored run_dijkstra method

--- app/basic_mouse.py
+++ app/basic_mouse.py
@@ -81,30 +81,30 @@ class BasicMouse(BaseMouse):
         visited = set()
 
         while heap:
-            (dist, current) = heapq.heappop(heap)
-
+            dist, current = heapq.heappop(heap)
             if current in visited:
                 continue
 
             visited.add(current)
-
             if current == self.goal:  # stop when the goal is found
                 break
 
             if dist != self.distances[current]:
                 continue
 
-            surroundings = self.inspect_surroundings()
-            for direction, cell in surroundings.items():
-                new_position = (current[0] + BaseMouse.MOVES[direction][0], current[1]
-                                + BaseMouse.MOVES[direction][1])
-                # only consider cells that are in the visited set and are not walls
-                if new_position in self.visited and new_position not in self.walls:
-                    alt_dist = self.distances[current] + 1
-                    if alt_dist < self.distances[new_position]:
-                        self.distances[new_position] = alt_dist
-                        self.predecessors[new_position] = current
-                        heapq.heappush(heap, (alt_dist, new_position))
+            self.update_distances_and_predecessors(heap, current, visited)
+
+    def update_distances_and_predecessors(self, heap, current, visited):
+        surroundings = self.inspect_surroundings()
+        for direction, cell in surroundings.items():
+            new_position = self.add_direction_to_position(current, BaseMouse.MOVES[direction])
+            # only consider cells that are in the visited set and are not walls
+            if new_position in self.visited and new_position not in self.walls:
+                alt_dist = self.distances[current] + 1
+                if alt_dist < self.distances[new_position]:
+                    self.distances[new_position] = alt_dist
+                    self.predecessors[new_position] = current
+                    heapq.heappush(heap, (alt_dist, new_position))
 
     def reconstruct_path(self):
         path = []
